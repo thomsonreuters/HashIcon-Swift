9c993cb61387a14a4bbba07a373bd717e6e092e3
Francisco Pereira <francisco.pereira@thomsonreuters.com>
2017-06-14 11:55:37 +0100

    add HashIconSwift-Swift

---
+++ Sources/HashIconSwift-Swift.h
@@ -0,0 +1,14 @@
+//
+//  HashIcon-Swift.h
+//  HashIconSwift
+//
+//  Created by Francisco Pereira on 14/06/2017.
+//  Copyright © 2017 Thomson Reuters. All rights reserved.
+//
+
+#ifndef HashIcon_Swift_h
+#define HashIcon_Swift_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* HashIcon_Swift_h */
