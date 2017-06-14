dd4ff143ae56bb5c06f02a80bdc701b5f8ccb006
Francisco Pereira <francisco.pereira@thomsonreuters.com>
2017-06-14 11:33:40 +0100

    fix Playground

--- HashIconSwift.playground/Contents.swift
+++ HashIconSwift.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import PlaygroundSupport
-import HashIconSwift
+// import HashIconSwift
 import UIKit
 
 var str = "Hello, playground"
@@ -9,6 +9,6 @@ var str = "Hello, playground"
 let view = UIView.init(frame: CGRect(x: 0, y: 0, width: 120, height: 120))
 view.backgroundColor = UIColor.yellow
 
-HashIcon(size: 5).drawIcon(input: "Example", container: view)
+// HashIcon(size: 5).drawIcon(input: "Example", container: view)
 
 view
